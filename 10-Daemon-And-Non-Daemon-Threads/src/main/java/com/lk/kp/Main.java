b4ce040231e4fbdb0848b186526240f32739cf24
kawindu2002 <kawindupriyashan@gmail.com>
2025-04-01 16:27:08 +0530

    create thread pool

---
+++ 10-Daemon-And-Non-Daemon-Threads/src/main/java/com/lk/kp/Main.java
@@ -0,0 +1,19 @@
+package com.lk.kp;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+//There are 2 types of threads.
+//
+//Daemon Threads = Service Threads
+//Non-Daemon Threads = user Threads
+//We creates User Threads in above modules.
+
+//Daemon threads provides service to user threads.
+//Non Daemon threads - Reading information and display output.
+//Daemon thread - Thread which are running behind the application in the background running threads , provide service to use threads.
+//JVM will stop all daemon threads if here is no running user threads.
+//
