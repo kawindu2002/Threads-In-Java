85c401f00961ea4db8a62dc91e551ec5445bbd84
kawindu2002 <kawindupriyashan@gmail.com>
2025-04-01 14:48:08 +0530

    set inter-thread-communication

--- 08-Inter-Thread-Communication/src/main/java/com/lk/kp/Main.java
+++ 08-Inter-Thread-Communication/src/main/java/com/lk/kp/Main.java
@@ -1,7 +1,34 @@
 package com.lk.kp;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        InputData inputData = new InputData();
+        PrintData printData = new PrintData();
 
+        Thread t1 = new Thread(() -> {
+        });
+
+        Thread t2 = new Thread(() -> {
+        });
     }
 }
+
+class PrintData{
+    int printNum = 0;
+    synchronized void print(){
+        System.out.println(printNum);
+    }
+}
+
+class InputData{
+    int inputNum = 0;
+    synchronized void inputNumber(){
+        Scanner input = new Scanner(System.in);
+        inputNum = input.nextInt();
+    }
+}
+
+
+
