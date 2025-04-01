e2418d341973ac46c54de116953e9da2a0d163e8
kawindu2002 <kawindupriyashan@gmail.com>
2025-04-01 14:21:31 +0530

    create module-8

--- 07-Synchronized-Keyword/src/main/java/com/lk/kp/Main.java
+++ 07-Synchronized-Keyword/src/main/java/com/lk/kp/Main.java
@@ -1,29 +1,40 @@
 package com.lk.kp;
 
 public class Main {
-    public static void main(String[] args) {
-        Thread t1 = new Thread(()-> {
-            System.out.println("T1");
-            increment(1000);
-
+    public static void main(String[] args) throws InterruptedException {
+        Calculation cal = new Calculation();
+        Thread t1 = new Thread(() -> {
+            for (int i = 1; i <= 1000; i++) {
+                cal.increment();
+            }
         });
 
-        Thread t2 = new Thread(()-> {
-            System.out.println("T2");
-            increment(1000);
-
+        Thread t2 = new Thread(() -> {
+            for (int i = 1; i <= 1000; i++) {
+                cal.increment();
+            }
         });
 
-        t1.setPriority(Thread.MAX_PRIORITY);
-        t1.setPriority(Thread.MIN_PRIORITY);
         t1.start();
         t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Value is: "+cal.num);
+    }
+}
+
+class Calculation{
+    int num;
+    public synchronized void increment(){
+       num++;
     }
 
-    public static void increment(int y){
-        for (int i = 0; i < y ; i++) {
-            System.out.println(i+1);
-        }
-    }
+//    Ekpr ekm resource ek threads kihipykin access krnn yddi deadlock awsthwt awith thread ekktwth resources access krnn briwenn puluwn.
+//    E wge problems solve krnn synchronized dnw...
+
+//    Output
+    //    Value is: 2000
 }
 
